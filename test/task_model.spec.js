f0d84d12b2513b7ae9d4741e473ebe11d78b2a10
LORD KAY <offeilord@gmail.com>
2018-06-26 11:47:24 +0000

    rename the libs file to lib.spec.js for clarity sake

--- test/task_model.spec.js
+++ test/task_model.spec.js
@@ -1,11 +1,6 @@
 //Tests for the task and it's label will appear here
 
-var User = require('../models/user'),
-	User_Verification = require('../models/user_verification'),
-	task   = require('../models/tasks'),
-	task_label = require('../models/task_label'),
-	mongoose = require('mongoose'),
-	config   = require('../config');
+
 
 	before(function(done){
 		mongoose.connect(config.database);
---
+++ test/utils/lib.spec.js
@@ -0,0 +1,6 @@
+var User = require('.../models/user'),
+	User_Verification = require('.../models/user_verification'),
+	task   = require('.../models/tasks'),
+	task_label = require('.../models/task_label'),
+	mongoose = require('mongoose'),
+	config   = require('.../config');
