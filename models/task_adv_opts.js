db680eb66d0030c87d73cae32076056611ee425e
LORD KAY <offeilord@gmail.com>
2018-06-27 10:52:44 +0000

    Create the schema for the advance options table

---
+++ models/task_adv_opts.js
@@ -0,0 +1,37 @@
+var mongoose = require('mongoose'),
+    Schema   = mongoose.Schema,
+    User     = require('./user'),
+    Task     = require('./tasks');
+
+var advanceOptionSchema = new Schema({
+    start_time:{
+        type:Date,
+        required: false
+    },
+    end_time:{
+        type:Date,
+        required:false
+    },
+    task_id:{
+        type: Schema.Types.ObjectId,
+        ref: 'Task'
+    },
+    user_id:{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+    createdAt:{
+        type: Date,
+        default: Date.now,
+    },
+    updatedAt:{
+        type: Date,
+        default: Date.now
+    }
+});
+
+// Defining the signals for the task advance options
+advanceOptionSchema.pre('save',function(next){
+    this.updatedAt(Date.now());
+    next();
+});
