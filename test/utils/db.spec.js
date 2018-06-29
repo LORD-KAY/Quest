76e9d5ec9a139a4c779512d908ccd17799ae88f7
LORD KAY <offeilord@gmail.com>
2018-06-29 12:59:20 +0000

    :tada: somethings are looking good

--- test/utils/db.spec.js
+++ test/utils/db.spec.js
@@ -2,13 +2,18 @@ var mongoose = require('mongoose');
 	
 	before(function(done){
 		  mongoose.connect('mongodb://127.0.0.1:27017/tasks');
-		  done();
+		  const db = mongoose.connection;
+		  db.on('error',console.error.bind(console, 'connection error'));
+		  db.once('open',function(){
+		  	console.log('Connection established');
+		  	done();
+		  });
 	});
 
 	after(function(done){
-		mongoose.connection.db.dropDatabase;
-		mongoose.connection.db.close;
-		done()
+		mongoose.connection.db.dropDatabase(function(){
+			mongoose.connection.close(done)
+		});
 	});
 
 	module.exports = function(){
