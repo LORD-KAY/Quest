b7c450987436286ac3057080368c0e39501b8814
LORD KAY <offeilord@gmail.com>
2018-06-29 11:20:14 +0000

    Add the first function to test

--- utils/resolvers.js
+++ utils/resolvers.js
@@ -1,1 +1,18 @@
 //requiring the necessary modules
+var bcrypt = require('bcryptjs'),
+    SALT_FACTOR  = 10;
+
+module.exports = {
+	hashPassword: function(password_data){
+		var password = password_data.toString();
+		bcrypt.genSalt(SALT_FACTOR,function(err,salt){
+			bcrypt.hash(password,salt,function(err,hash_password){
+				console.log(hash_password);
+				return hash_password;
+			});
+		});
+	},
+	validatePassword: function(password, confirm_password){
+		
+	}
+}
