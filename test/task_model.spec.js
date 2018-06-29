f47f3b6c72fe6d677f20e44cd1fab2a46281b020
LORD KAY <offeilord@gmail.com>
2018-06-29 12:58:46 +0000

    Testing is looking good so far

--- test/task_model.spec.js
+++ test/task_model.spec.js
@@ -1,25 +1,55 @@
 // Testing the models 
-var expect = require('chai').expect;
+var expect = require('chai').expect,
+	should = require('chai').should;
 var User = require('../models/user'),
 	User_Verification = require('../models/user_verification'),
 	task   = require('../models/tasks'),
 	task_label = require('../models/task_label'),
-	hash_password = require('./utils/bcrypt.spec');
+	Resolvers  = require('../utils/resolvers');
 var db = require('./utils/db.spec');
+	
+	describe(' # Checking the resolvers ',function(){
+		it('should return a string - hash',function(done){
+			expect(Resolvers.hashPassword).to.be.a('function');
+			Resolvers.hashPassword('lordbanks').then( hashed => {
+				expect(hashed).to.be.a('string');
+			});
+			done();
+		});
 
-	describe("Signing up new users", function(){
+		it('should return true',function(done){
+			var data = Resolvers.comparePassword('lordbanks','lordbanks');
+			expect(data).to.be.true;
+			done();
+		});
+	});
+
+	describe("Testing models", function(){
 		db();
-		describe(" #Saving data",function(){
+		describe(" #Saving a user",function(){
 			it("should return no error",function(done){
-				var query = {
-					fullname: 'Acheampong Lord Offei',
-					email: 'lordkay1996@gmail.com',
-					password: password,
-					confirm_password: password
-				};
+				Resolvers.hashPassword('lordbanks').then(hashed => {
+					var query = {
+						fullname: 'Lord Ray',
+						email: 'offeilord@gmail.com',
+						password: hashed,
+						confirm_password: hashed
+					};
 
-				var data = new User(query);
-				data.save();
+					var data = new User(query);
+					data.save();
+				});
+				done();
+			});
+		});
+
+		describe(" #Finding a user",function(){
+			it('should return an identified user',function(done){
+				var data = User.findOne({ email: 'offeilord@gmail.com' });
+				data.exec(function(err,data){
+					expect(data).to.be.an('object');
+					expect(data).to.have.property('fullname');
+				});
 				done();
 			});
 		});
