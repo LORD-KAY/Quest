edbe6948ae5bd3c467f12c7e5457873f9b225567
LORD KAY <offeilord@gmail.com>
2018-07-03 11:13:51 +0000

    Add a remove() signal to the task model

--- models/tasks.js
+++ models/tasks.js
@@ -1,4 +1,6 @@
-var User = require('./user')
+var User = require('./user');
+var Task_Label = require('./task_label');
+var Task_Advance_Options = require('./task_adv_opts');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
@@ -28,10 +30,15 @@ var task = new Schema({
 });
 
 // Creating signals for the tables
-// task.pre('save',function(next){
-// 	this.updatedAt(Date.now())
-// 	next();
-// });
+task.pre('save',function(next){
+
+});
+task.pre('remove',function(next){
+    Task_Label.remove({ task_id: this._id }).exec();
+    Task_Advance_Options.remove({ task_id: this._id }).exec();
+    next();
+});
+
 
 var Task = mongoose.model('Task',task);
 module.exports = Task;
